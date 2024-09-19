17d8c11e2d156a6598b700b409bd01031c9ffc1e
diana-rahma <dianarhm12@gmail.com>
2024-09-19 08:51:34 +0700

    perbaikan pratikum 1 - variabel konstanta

--- variabel_konstanta.php
+++ variabel_konstanta.php
@@ -3,7 +3,7 @@ $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
-
+echo "<br>";
 $benar = true;
 $salah = false;
 echo "Variabel benar:  $benar, Variabel salah:  $salah";
@@ -11,6 +11,6 @@ echo "Variabel benar:  $benar, Variabel salah:  $salah";
 // Mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS", "Websiteku.com");
 define("TAHUN_PENDIDIKAN", 2024);
-
+echo "<br>";
 echo "Selamat datang di " . NAMA_SITUS . ", situs yang didirikan pada tahun " . TAHUN_PENDIDIKAN . ","; 
 ?>
